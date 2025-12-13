76b3a2e94369f0b684a4d7adafaab9f68d82690c
Yevhenii <evhenii.rybin@gmail.com>
2025-12-13 17:01:21 +0200

    fix auth

--- plugins/initializeAuth.client.ts
+++ plugins/initializeAuth.client.ts
@@ -1,14 +1,12 @@
 import { defineNuxtPlugin } from "#app";
 import { useAuthStore } from "~~/stores/auth";
-import { mockAdmin } from "~~/utils/mockData";
+import { mockPartners } from "~~/utils/mockData";
 
 export default defineNuxtPlugin(() => {
   const authStore = useAuthStore();
-  
-  // Ініціалізуємо користувача як адміністратора за замовчуванням для демо
-  // Перевіряємо, чи користувач не встановлений або не є адміністратором
-  if (!authStore.currentUser || authStore.currentUser.role !== "admin") {
-    authStore.login(mockAdmin);
+  // Ініціалізуємо користувача як партнера за замовчуванням для демо
+  if (!authStore.currentUser || authStore.currentUser.role !== "partner") {
+    authStore.login(mockPartners[0]);
   }
 });
 
