3a4224d7f50caed2a82b6d8e43a61f5fb4112f58
Vitalii Bohdaniuk <vitaliibogdaniuk@MacBook-Pro-Vitalii-4.local>
2025-12-14 03:19:06 +0200

    change favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,13 @@ export default defineNuxtConfig({
     pageTransition: { name: "fade", mode: "out-in" },
     head: {
       titleTemplate: "DeepWater",
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon_32.png" },
+        { rel: "icon", type: "image/png", sizes: "64x64", href: "/favicon_64.png" },
+        { rel: "icon", type: "image/png", sizes: "128x128", href: "/favicon_128.png" },
+        { rel: "apple-touch-icon", sizes: "256x256", href: "/favicon_256.png" },
+      ],
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1.0" },
